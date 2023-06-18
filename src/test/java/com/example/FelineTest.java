33d34c71ae40fce519e5b5d03bef1a17697fd0df
Maria Semenova <semenovamn@live.ru>
2023-06-18 10:25:06 +0400

    clean unused impoort

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,15 +1,11 @@
 package com.example;
 
 import junit.framework.TestCase;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FelineTest extends TestCase {
